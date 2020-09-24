c07de461545340778c7a29cc87056e2fd745b29e
atleastzero <megan.obryan52@gmail.com>
2020-09-24 16:49:45 -0700

    Created createCharacter function

--- contracts/CharacterFactory.sol
+++ contracts/CharacterFactory.sol
@@ -8,10 +8,27 @@ contract CharacterFactory {
         Cyborg
     }
 
+    event NewCharacter(string name, Tribe tribe);
+
     struct Character {
         string name;
         Tribe tribe;
-        uint64 defense;
-        uint64 attack;
+        uint defense;
+        uint attack;
+    }
+
+    Character[] public characters;
+
+    mapping (address => Character) public ownerToCharacter;
+
+    function createCharacter(string memory _name, Tribe _tribe) public {
+        require(ownerToCharacter[msg.sender].defense == 0);
+        Character storage character = ownerToCharacter[msg.sender];
+        character.name = _name;
+        character.tribe = _tribe;
+        character.defense = 1;
+        character.attack = 1;
+        characters.push(character);
+        emit NewCharacter(_name, _tribe);
     }
 }
